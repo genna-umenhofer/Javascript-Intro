98fc0d24d58772abd082bd433b7d172dc8bb5996
Genna Umenhofer <gumenhofer@gmail.com>
2022-04-05 06:38:20 -0500

    Exercises 1-4

---
+++ Preparations.js
@@ -0,0 +1,14 @@
+// Exercise 1-4
+
+console.log("This is file one");
+
+let foo = "bar";
+console.log(foo);
+foo;
+      /*
+        1. This code outputs the string "bar"
+        2.
+        3. The Chrome REPL shows a SyntaxError that 'foo' has already been identified. This is
+        because the code above is running first and then the code I copied is running. If I comment
+        out the code above, then it shows 'bar'.
+      */
