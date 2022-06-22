c622781632f3ef86110f958b3879b56565b5d8ea
Genna Umenhofer <gumenhofer@gmail.com>
2022-06-21 20:52:13 -0500

    Finished Preparations

--- Preparations.js
+++ Preparations.js
@@ -7,8 +7,65 @@ console.log(foo);
 foo;
       /*
         1. This code outputs the string "bar"
-        2.
+        2. This code outputs the string 'bar' as well as bar. The first is from the console.log() line and the second is from the foo line.
         3. The Chrome REPL shows a SyntaxError that 'foo' has already been identified. This is
         because the code above is running first and then the code I copied is running. If I comment
         out the code above, then it shows 'bar'.
       */
+
+// Exercise 5
+/*
+Subrsting - String Constructor - Instance Method (Prototype)
+Create - Document Constructor - Static Method
+fromCharCode - String Constructor - Static Method
+slice - Array Constructor - Instance Method (Prototype)
+toString - Object Constructor - Instance Method (Prototype)
+*/
+
+// Exercise 6
+/*
+index - This satisfies the style guidelines for naming a variable.
+CatName - The first letter should be lowercase - catName
+snake_case - Remove the underscore and capitalize the C - snakeCase
+lazyDog - This satisfies the style guidelines for naming a variable.
+quick_Fox - Remove the underscore - quickFox
+1stCharacter - Names cannot begin with a digit.
+operand2 - This satisfies the style guidelines for naming a variable.
+BIG_NUMBER - Remove the underscore and change to lowercase - bigNumber
+*/
+
+// Exercise 7
+/*
+index - Capitalize - Index
+CatName - This satisfies the style guidelines for naming a function.
+snake_case - Remove the underscore and capitalize - SnakeCase
+lazydog - Capitalize the L - LazyDog
+quick_Fox - Remove the underscore and capitalize - QuickFox
+1stCharacter - Names cannot begin with a digit.
+operand2 - Capitalize - Operand2
+BIG_NUMBER - Remove the underscore and make lowercase - BigNumber
+*/
+
+// Exercise 8
+/*
+index - Capitalize- INDEX
+CatName - Capitalize and add an underscore - CAT_NAME
+snake_case - Capitalize - SNAKE_CASE
+lazyDog - Capitalize and add an underscore - LAZY_DOG
+quick_Fox - Capitalize - QUICK_FOX
+1stCharacter - Names cannot begin with a digit.
+operand2 - Capitalize and add an underscore - OPERAND2
+BIG_NUMBER - This satisfies the style guidelines for naming a magic number.
+*/
+
+// Exercise 9
+/*
+index - This satisfies the style guidelines for naming variable, functions, or constants.
+CatName - This satisfies the style guidelines for naming variable, functions, or constants.
+snake_case - Needs to change 
+lazyDog - This satisfies the style guidelines for naming variable, functions, or constants.
+quick_Fox - Needs to change 
+1stCharacter - Names cannot begin with a digit.
+operand2 - This satisfies the style guidelines for naming variable, functions, or constants.
+BIG_NUMBER - This satisfies the style guidelines for naming variable, functions, or constants.
+*/
