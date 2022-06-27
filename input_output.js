e8e408480b815d6be493bf8afca266fdc91862f2
Genna Umenhofer <gumenhofer@gmail.com>
2022-06-27 12:19:40 -0500

    Completed I/O Exercises

---
+++ input_output.js
@@ -0,0 +1,17 @@
+// Exercise 1
+let name = prompt("What is your name? ");
+console.log(`Hello, ${name}!`);
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Exercise 2
+let firstName = prompt("What is your first name? ");
+let lastName = prompt("What is your last name? ");
+console.log(`Hello, ${firstName} ${lastName}!`);
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Exercise 3
+let age = prompt("What is your age? ");
+console.log(`You are ${age} years old.`);
+console.log(`In 10 years, you will be ${Number(age) + 10} years old.`);
+console.log(`In 20 years, you will be ${Number(age) + 20} years old.`);
+console.log(`In 30 years, you will be ${Number(age) + 30} years old.`);
+console.log(`In 40 years, you will be ${Number(age) + 40} years old.`);
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
